13c3f95770aececfd9c9938fa5a8ded129c793a9
+Ter <7396589+masterPlusTer@users.noreply.github.com>
2024-12-30 10:09:28 -0400

    Update exampleESPNOWemisor.py

--- exampleESPNOWemisor.py
+++ exampleESPNOWemisor.py
@@ -1,15 +1,15 @@
-import ESPNOW
+import connectivity
 import time
 
 # Dirección MAC del receptor (cambia esto con la dirección MAC real del receptor)
 mac_receptor = b'\x00\x00\x00\x00\x00\x00'  # Reemplaza con la MAC real
 mensaje = "Hola desde el esp32-s3-geek!!!"
 
-ESPNOW.init_espnow()
+connectivity.init_espnow()
 
 
 while True:
-    ESPNOW.enviar_mensaje(mac_receptor, mensaje)
+    connectivity.enviar_mensaje(mac_receptor, mensaje)
     print(f"Mensaje enviado: {mensaje}")
     time.sleep(5)
 
